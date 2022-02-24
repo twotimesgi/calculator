e055a3bd9a5c5d4d64f1c21ac17c4cbd4eadbfb7
twotimesgi <luigibardellagerbi@mail.com>
2022-02-24 23:09:57 +0100

    Fix effetti

--- script.js
+++ script.js
@@ -28,41 +28,18 @@ for(let i = 0; i < nums.length; i++){
   });
 }
 
-opers[6].addEventListener("click", function(){
-	n = m;
-  m = "";
-  res.innerHTML = "0";
-  currentOp = "+";
-  opers[6].style.background = "white"
-  opers[6].style.color = "#ff911c";
-});
+for(let i = 3; i < 7; i++){
+  opers[i].addEventListener("click", function(){
+    n = m;
+    m = "";
+    res.innerHTML = "0";
+    currentOp = opers[i].dataset.func.toString();
+    console.log(currentOp);
+    opers[i].style.background = "white"
+    opers[i].style.color = "#ff911c";
+  });
+}
 
-opers[5].addEventListener("click", function(){
-	n = m;
-  m = "";
-  res.innerHTML = "0";
-  currentOp = "-";
-  opers[5].style.background = "white"
-  opers[5].style.color = "#ff911c";
-});
-
-opers[4].addEventListener("click", function(){
-	n = m;
-  m = "";
-  res.innerHTML = "0";
-  currentOp = "x";
-  opers[4].style.background = "white"
-  opers[4].style.color = "#ff911c";
-});
-
-opers[3].addEventListener("click", function(){
-	n = m;
-  m = "";
-  res.innerHTML = "0";
-  currentOp = "÷";
-  opers[3].style.background = "white"
-  opers[3].style.color = "#ff911c";
-});
 opers[7].addEventListener("click", function(){
 	if(currentOp == "+"){
   	  res.innerHTML = parseFloat(n) + parseFloat(m);
@@ -100,9 +77,13 @@ opers[0].addEventListener("click", function(){ //C
   opers[6].style.background = "#ff911c"
   opers[6].style.color = "white";
   currentOp = "";
+  opers[0].style.animation = "flash 0.2s linear 1";
+  opers[0].addEventListener("animationend",function(){
+    opers[0].style.animation = "none";
+  });
 });
 
-opers[1].addEventListener("click", function(){
+opers[1].addEventListener("click", function(){ //+/-
   if(res.innerHTML[0] != "-" && res.innerHTML[0] != "0"){
     res.innerHTML = "-" + res.innerHTML;
     m = "-" + m;
@@ -110,13 +91,20 @@ opers[1].addEventListener("click", function(){
       m =  m.slice(1,res.innerHTML.length);
       res.innerHTML = res.innerHTML.slice(1,res.innerHTML.length);
   }
+  opers[1].style.animation = "flash 0.2s linear 1";
+  opers[1].addEventListener("animationend",function(){
+    opers[1].style.animation = "none";
+  });
 });
 
-opers[2].addEventListener("click", function(){
+opers[2].addEventListener("click", function(){ //%
   m = m / 100;
   res.innerHTML = res.innerHTML/100;
+  opers[2].style.animation = "flash 0.2s linear 1";
+  opers[2].addEventListener("animationend",function(){
+    opers[2].style.animation = "none";
+  });
 });
-
 
 
 
